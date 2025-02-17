cf2959921d25acdf89c114ca1cf5cdf1201e6110
Harish Kumar <harishkumar27b@gmail.com>
2025-02-17 17:34:47 +0530

    Update App.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,13 +3,12 @@ import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import SignUp from "./components/SignUp";
 import Profile from "./components/Profile";
 import axios from "axios";
-import './assets/style.css'; // Make sure the style is properly applied from the CSS file.
+import './assets/style.css'; 
 
 const App = () => {
   const [userData, setUserData] = useState(null);
 
   const handleFormSubmit = (formData) => {
-    // Save the submitted data to local state
     setUserData(formData);
   };
 
